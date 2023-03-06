acf0fb083582c61bab487a7a2b7c993cbe53fda9
LeoBarreiro <a.leo.barreiro@gmail.com>
2023-03-06 20:53:11 -0300

    feat: wsl install scripts

---
+++ scripts/01-shell.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#set -x
+
+sudo apt update -y
+
+if [[ ! $(which zsh) ]]; then
+    echo "ZSH"
+    sudo apt install zsh -y
+fi
+echo "[ OK ] ZSH"
+
+if [[ ! -d ~/.oh-my-zsh/ ]]; then
+    echo "ZSH Oh my zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+fi
+echo "[ OK ] ZSH Oh my zsh"
+
+DIR=~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+if [[ ! -d "$DIR" ]]; then
+    echo "ZSH Syntax highlighting"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+echo "[ OK ] ZSH Syntax highlighting"
+
+DIR=~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+if [[ ! -d "$DIR" ]]; then
+    echo "ZSH Autosuggestion"
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+        ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+echo "[ OK ] ZSH Autosuggestion"
+
+if [[ ! $SHELL == /usr/bin/zsh && ! $SHELL == /bin/zsh ]]; then
+    echo "ZSH by default"
+    chsh -s /bin/zsh
+fi
+echo "[ OK ] ZSH by default"
---
+++ scripts/02-terminal.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#set -x
+
+sudo apt update -y
+
+if [[ ! $(which starship) ]]; then
+    echo "starship"
+    curl -sS https://starship.rs/install.sh | sh
+fi
+echo "[ OK ] starship"
+
+if [[ ! $(which tmux) ]]; then
+    echo "tmux"
+    sudo apt install tmux -y
+fi
+echo "[ OK ] tmux"
+
+if [[ ! $(which lsd) ]]; then
+    echo "lsd"
+    wget -O lsd_amd64.deb https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb
+    sudo dpkg -i lsd_amd64.deb
+fi
+echo "[ OK ] lsd"
+
+if [[ ! $(which batcat) ]]; then
+    echo "batcat"
+    sudo apt install bat -y
+    echo "In ubuntu bat is batcat due to a name clash with another package: alias to bat"
+    mkdir -p ~/.local/bin
+    ln -s /usr/bin/batcat ~/.local/bin/bat
+fi
+echo "[ OK ] batcat"
+
+if [[ ! $(which fzf) ]]; then
+    echo "fzf"
+    sudo apt install fzf -y
+fi
+echo "[ OK ] fzf"
